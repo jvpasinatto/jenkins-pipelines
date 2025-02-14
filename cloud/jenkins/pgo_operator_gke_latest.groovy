75ce58b59e5413c9904520def86693774ff81284
Julio Pasinatto <julio.pasinatto@percona.com>
2025-02-14 18:45:17 -0300

    +debug

--- cloud/jenkins/pgo_operator_gke_latest.groovy
+++ cloud/jenkins/pgo_operator_gke_latest.groovy
@@ -234,8 +234,11 @@ void runTest(Integer TEST_ID) {
                     export PATH="\${KREW_ROOT:-\$HOME/.krew}/bin:\$PATH"
 
                     kubectl kuttl test --config e2e-tests/kuttl.yaml --test "^$testName\$" --report xml
+                    ls
                     pwd
                     cp kuttl-report.xml ../
+                    cd ..
+                    ls
                 """
             }
             pushArtifactFile("$GIT_BRANCH-$GIT_SHORT_COMMIT-$testName-$USED_PLATFORM_VER-$PPG_TAG-CW_$CLUSTER_WIDE-$PARAMS_HASH")
@@ -421,6 +424,7 @@ pipeline {
         always {
             echo "CLUSTER ASSIGNMENTS\n" + tests.toString().replace("], ","]\n").replace("]]","]").replaceFirst("\\[","")
             sh "pwd"
+            sh "ls"
             step([$class: 'JUnitResultArchiver', testResults: '*.xml', healthScaleFactor: 1.0])
             archiveArtifacts '*.xml'
 
